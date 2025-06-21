4800712c48aada032bc281308b1f0b03b226f323
astrophelc <1187050027@student.uinsgd.ac.id>
2025-06-21 11:41:50 +0700

    Add Rich Text Editor

--- resources/views/components/posts/edit.blade.php
+++ resources/views/components/posts/edit.blade.php
@@ -1,3 +1,6 @@
+@push('style')
+    <link href="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.snow.css" rel="stylesheet" />
+@endpush
 <!-- Modal content -->
 <div class="max-w-4xl relative p-4 bg-white rounded-lg border dark:bg-gray-800 sm:p-5">
     <!-- Modal header -->
@@ -5,7 +8,7 @@
         <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Edit Post</h3>
     </div>
     <!-- Modal body -->
-    <form action="/dashboard/{{ $post->slug }}" method="POST">
+    <form action="/dashboard/{{ $post->slug }}" method="POST" id="post-form">
         @csrf
         @method('PATCH')
         <div class="mb-4">
@@ -33,8 +36,9 @@
         <div class="mb-4">
             <label for="body" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Body</label>
             <textarea id="body" name="body" rows="4"
-                class="@error('body') bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 @enderror block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                class="hidden @error('body') bg-red-50 border border-red-500 text-red-900 placeholder-red-700 focus:ring-red-500 focus:border-red-500 @enderror block p-2.5 w-full text-sm text-gray-900 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                 placeholder="Write post body here">{{ old('body') ?? $post->body }}</textarea>
+            <div id="editor">{!! old('body', $post->body) !!}</div>
             @error('body')
                 <p class="mt-2 text-xs text-red-600 dark:text-red-500"><span class="font-medium">{{ $message }}</p>
             @enderror
@@ -51,3 +55,27 @@
         </div>
     </form>
 </div>
+@push('script')
+    <script src="https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js"></script>
+    <script>
+        const quill = new Quill('#editor', {
+            theme: 'snow',
+            placeholder: 'Write post body here'
+        });
+
+        const postForm = document.querySelector('#post-form');
+        const postBody = document.querySelector('#body');
+        const quillEditor = document.querySelector('#editor');
+
+        postForm.addEventListener('submit', function(e) {
+            e.preventDefault()
+
+            const content = quillEditor.children[0].innerHTML;
+            // console.log(content)
+
+            postBody.value = content;
+
+            this.submit();
+        });
+    </script>
+@endpush
--- resources/views/components/posts/show.blade.php
+++ resources/views/components/posts/show.blade.php
@@ -34,7 +34,7 @@
                 </address>
                 <div class="flex justify-between items-center">
                     <div class="flex items-center space-x-3 sm:space-x-4">
-                        <button type="button"
+                        <a href="/dashboard/{{ $post->slug }}/edit"
                             class="text-white inline-flex items-center bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 cursor-pointer">
                             <svg aria-hidden="true" class="mr-1 -ml-1 w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -45,7 +45,7 @@
                                     clip-rule="evenodd" />
                             </svg>
                             Edit
-                        </button>
+                        </a>
                         <button type="button"
                             class="inline-flex items-center text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900 cursor-pointer">
                             <svg aria-hidden="true" class="w-5 h-5 mr-1.5 -ml-1" fill="currentColor" viewbox="0 0 20 20"
@@ -62,7 +62,9 @@
                     class="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
                     {{ $post['title'] }}</h1>
             </header>
-            <p>{{ $post['body'] }}</p>
+            <div>
+                {{ $post['body'] }}
+            </div>
         </article>
     </div>
 </main>
